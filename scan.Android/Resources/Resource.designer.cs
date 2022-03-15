185c59b8de9134ea169c117170cb346beccb4fe6
xv710 <jcp710@hotmail.com>
2022-03-15 09:31:39 +0100

    permisos

--- scan.Android/Resources/Resource.designer.cs
+++ scan.Android/Resources/Resource.designer.cs
@@ -16667,22 +16667,19 @@ namespace scan.Droid
 			public const int qrcode = 2131165326;
 			
 			// aapt resource value: 0x7F07008F
-			public const int qrcode2 = 2131165327;
+			public const int recordatorios = 2131165327;
 			
 			// aapt resource value: 0x7F070090
-			public const int recordatorios = 2131165328;
+			public const int scan = 2131165328;
 			
 			// aapt resource value: 0x7F070091
-			public const int scan = 2131165329;
+			public const int test_custom_background = 2131165329;
 			
 			// aapt resource value: 0x7F070092
-			public const int test_custom_background = 2131165330;
+			public const int tooltip_frame_dark = 2131165330;
 			
 			// aapt resource value: 0x7F070093
-			public const int tooltip_frame_dark = 2131165331;
-			
-			// aapt resource value: 0x7F070094
-			public const int tooltip_frame_light = 2131165332;
+			public const int tooltip_frame_light = 2131165331;
 			
 			static Drawable()
 			{
