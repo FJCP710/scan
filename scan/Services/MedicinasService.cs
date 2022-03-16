73a4d64b32be14d4f52c6ae322c0f09faa83c9f4
xv710 <jcp710@hotmail.com>
2022-03-16 11:58:07 +0100

    services y métodos consultas

---
+++ scan/Services/MedicinasService.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace scan.Services
+{
+    public static class MedicinasService
+    {
+        static async Task Init()
+        {
+
+        }
+
+        public static async Task AddMedicina(string id, string nombre)
+        {
+
+        }
+
+        public static async Task CreateNotification(string nombre, int horas)
+        {
+
+        }
+
+        public static async Task CreateReminder(string nombre, DateTime fecCaduc)
+        {
+
+        }
+    }
+}
