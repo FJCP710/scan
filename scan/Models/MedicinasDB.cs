fecff0848b7ac7dd3e282f3a720dad3be91cfd88
xv710 <jcp710@hotmail.com>
2022-03-19 09:47:45 +0100

    bbdd

--- scan/Models/MedicinasDB.cs
+++ scan/Models/MedicinasDB.cs
@@ -16,5 +16,17 @@ namespace scan.Services
         public bool sns { get;}
         public DateTime fecCaduc { get; set; }
         public int horas { get; set; }
+
+        public MedicinasDB(string id, string nombre, string prospecto, int mg, bool prescripcion, bool sns, DateTime fecCaduc, int horas)
+        {
+            this.id = id;
+            this.nombre = nombre;
+            this.prospecto = prospecto;
+            this.mg = mg;
+            this.prescripcion = prescripcion;
+            this.sns = sns;
+            this.fecCaduc = fecCaduc;
+            this.horas = horas;
+        }
     }
 }
--- scan/Services/MedicinasService.cs
+++ scan/Services/MedicinasService.cs
@@ -1,30 +1,43 @@
-﻿using System;
+﻿using SQLite;
+using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Text;
 using System.Threading.Tasks;
+using Xamarin.Essentials;
 
 namespace scan.Services
 {
     public static class MedicinasService
     {
+        static SQLiteAsyncConnection db;
+        static MedicinasDB medicinas;
         static async Task Init()
         {
+            if (db == null) return;
 
+            var databasePath = Path.Combine(FileSystem.AppDataDirectory, "MyData.db");
+
+            db = new SQLiteAsyncConnection(databasePath);
+
+            
         }
 
-        public static async Task AddMedicina(string id, string nombre)
+        public static async Task AddMedicina(MedicinasDB medicinas)
         {
-
+            await Init();
+            
+            
         }
 
-        public static async Task CreateNotification(string nombre, int horas)
+        public static async Task CreateNotification(MedicinasDB medicinas)
         {
-
+            await Init();
         }
 
-        public static async Task CreateReminder(string nombre, DateTime fecCaduc)
+        public static async Task CreateReminder(MedicinasDB medicinas)
         {
-
+            await Init();
         }
     }
 }
